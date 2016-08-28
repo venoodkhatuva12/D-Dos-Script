9ba39b5f6e1ecf432de6e9ccfe817b4687841fa3
Venood Katuwa <vinod.katuwa12@gmail.com>
2016-08-29 01:43:38 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#Script for D-Dos
+#Author: Vinod.N K
+#Usage: D-Dos Automation Script
+#Distro : Linux -Centos, Rhel, and any fedora
+#Check whether root user is running the script
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo "Installing D-dos script..."
+if [ -d '/usr/local/ddos' ]; then
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
+else
+	mkdir /usr/local/ddos
+fi
+clear
+echo; echo 'Installing DOS-Deflate 0.6'; echo
+echo; echo -n 'Downloading source files...'
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+echo -n '.'
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+echo -n '.'
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+echo -n '.'
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
+echo '...done'
+
+echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+echo
+cat /usr/local/ddos/LICENSE | less
+
+#################################
+#uninstall script
+#################################
+
+#!/bin/sh
+echo; echo "Uninstalling DOS-Deflate"
+echo; echo; echo -n "Deleting script files....."
+
+if [ -e '/usr/local/sbin/ddos' ]; then
+	rm -f /usr/local/sbin/ddos
+	echo -n ".."
+
+fi
+if [ -d '/usr/local/ddos' ]; then
+	rm -rf /usr/local/ddos
+	echo -n ".."
+
+fi
+echo "done"
+echo; echo -n "Deleting cron job....."
+if [ -e '/etc/cron.d/ddos.cron' ]; then
+	rm -f /etc/cron.d/ddos.cron
+
+	echo -n ".."
+fi
+echo "done"
+echo; echo "Uninstall Complete"; echo
